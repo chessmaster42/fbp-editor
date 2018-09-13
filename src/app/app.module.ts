f9adedd34410e560cf459bc0b6aa5e8013bc8bb1
Joseph Pearson <chessmaster42@gmail.com>
2018-09-13 11:12:44 -0500

    Added initial angular project files

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FlexLayoutModule,
+        HttpClientModule,
+        RouterModule.forRoot([]),
+    ],
+    bootstrap: [
+        AppComponent
+    ]
+})
+export class AppModule {}
